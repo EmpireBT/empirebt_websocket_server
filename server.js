d16a375a942d5e478d0ba3ee1e7b9599f06f49f1
Manuel Aude Morales <maude@securebt.com>
2014-04-03 17:55:23 -0600

    Fixed server.js from merge connflict

--- server.js
+++ server.js
@@ -5,14 +5,8 @@ var app = require('http').createServer(),
     amqp = require('amqplib'),
     fs = require('fs');
 
-<<<<<<< HEAD
 var settings = JSON.parse(fs.readFileSync('config.json').toString());
-=======
-var settings = {
-  BASE_URL : 'http://10.20.217.29:8000/',
-  PORT : '8000'
-};
->>>>>>> f90353df1b4bddc8e0c4207856c0ac9ecb60f51c
+
 
 var webservices_clients = {
   authorization : require('./webservices_clients/authorization')(settings),
@@ -20,12 +14,8 @@ var webservices_clients = {
   battle : require('./webservices_clients/battle')(settings)
 };
 var amqp_connection = null;
-<<<<<<< HEAD
-app.listen(settings.port);
-=======
-app.listen(settings.PORT);
->>>>>>> f90353df1b4bddc8e0c4207856c0ac9ecb60f51c
 
+app.listen(settings.port);
 
 io.configure(function () {
   io.set('authorization', webservices_clients.authorization.general);
